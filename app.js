6ef92ae7484a151b6536c6c83d9e99cdd7a0b4d7
Ray Jui-Chung Wu <rayjcwu@gmail.com>
2013-08-03 20:50:05 -0700

    add ntwitter for twitter streaming api

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , fs = require('fs')
-  , path = require('path');
+  , path = require('path')
+  , ntwitter = require('ntwitter');
 
+var twit = new ntwitter({
+  consumer_key: 'y2it3VGcR0lpmfplJWyvQ',
+  consumer_secret: '0ZARGMogR6lGCDMli8eyrh2PhOm9U5WhhjHM1nz0Hzs',
+  access_token_key: '1615787012-I7RTCotqwBUauGIQo7ZGS6VvfoKIKckuI0E8tUk',
+  access_token_secret: 'fJzic3iSklcsIaeFBJqsFnioxLxYLxf0BusheqwMc'
+});
+
+twit
+  .stream('statuses/sample', function(stream){
+    stream.on('data', function(data){
+      console.log(data);
+    });
+  });
 var app = require('express')();
 
 module.exports = app;
